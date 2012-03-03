0953324a7d1d67d147087fb5ab28982dfa03347e
Arne Coucheron <arco68@gmail.com>
2012-03-03 22:53:06 +0100

    Fix inheritance of PRODUCT_PROPERTY_OVERRIDES for CM audio files
    Otherwise the ones set in CM vendor will be overridden by these.
    
    Change-Id: I3f87dfd009d8ddfd48972c15770599742b12c4af

--- target/product/core.mk
+++ target/product/core.mk
@@ -18,11 +18,7 @@ PRODUCT_BRAND := generic
 PRODUCT_DEVICE := generic
 PRODUCT_NAME := core
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.notification_sound=OnTheHunt.ogg \
-    ro.config.alarm_alert=Alarm_Classic.ogg
-
-PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES := \
     ApplicationsProvider \
     BackupRestoreConfirmation \
     Browser \
