160438b1fb68feba2056c04819d589c86f329a43
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-07-14 14:29:59 +0159

    build/core: Fix build errors
    - Include task hooks only if they exists
    - Fix typos
    
    Change-Id: Ide3b1bd0bac050da5b96d1e9a0dabd55fbf675fe
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- core/legacy_prebuilts.mk
+++ core/legacy_prebuilts.mk
@@ -103,4 +103,15 @@ GRANDFATHERED_ALL_PREBUILT := \
 	ueventd.rc \
 	ueventd.stingray.rc \
 	vold.fstab \
-	wl1271.bin
+	wl1271.bin \
+	zoneinfo.dat \
+	zoneinfo.idx \
+	zoneinfo.version
+
+ifeq ($(TARGET_BOOTLOADER_TYPE),uboot)
+GRANDFATHERED_ALL_PREBUILT += \
+	build-uboot \
+	build-mkenvimg
+endif
+
+>>>>>>> 920b36b... build/core: Fix build errors
